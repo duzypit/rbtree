84a61c122cf4e7080426321a6934a154e8eeeac5
duzy_pit <piatek.tomasz@gmail.com>
2018-12-02 18:43:49 +0100

    removed definitions

--- include/RBTree.hpp
+++ include/RBTree.hpp
@@ -1,41 +1,21 @@
 #ifndef __BWTREE_HPP__
 #define __BWTREE_HPP__
 #include "VTree.hpp"
+#include "Node.hpp"
 
 template<typename T>
 class RBTree
 {
-    std::unique_ptr<Node<T>> head = nullptr;
+    std::shared_ptr<Node<T>> head = nullptr;
 
 public:
-    RBTree()
-    {
-
-    }
-
-    ~RBTree(){
-
-    }
-
-    void insert(T i)
-    {
-
-    }
-
-    void remove(T r)
-    {
-
-    }
-
-    void destroy_tree()
-    {
-
-    }
-
-    void search(T s)
-    {
-
-    }
+    RBTree();
+    RBTree(T value);
+    ~RBTree();
+    void insert(T i);
+    void remove(T r);
+    void destroy_tree();
+    Node<T>* search(T value);
 };
 #endif
 
